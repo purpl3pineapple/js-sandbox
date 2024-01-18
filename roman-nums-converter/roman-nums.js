0bf4c0a4beae28079f638195cab330e5e6c1336e
purpl3pineapple <opaloctopus1005@gmail.com>
2024-01-18 05:23:10 -0500

    roman numeral converter using recursion

--- roman-nums-converter/roman-nums.js
+++ roman-nums-converter/roman-nums.js
@@ -1,73 +1,123 @@
-const one = { value: 1, roman: "I" };
-const four = { value: 4, roman: "IV" };
-const five = { value: 5, roman: "V" };
-const nine = { value: 9, roman: "IX" };
-const ten = { value: 10, roman: "X" };
-const forty = { value: 40, roman: "XL" };
-const fifty = { value: 50, roman: "L" };
-const ninety = { value: 90, roman: "XC" };
-const hundred = { value: 100, roman: "C" };
-const fourHundred = { value: 400, roman: "CD" };
-const fiveHundred = { value: 500, roman: "D" };
-const nineHundred = { value: 900, roman: "CM" };
-const thousand = { value: 1000, roman: "M" };
-
-const roman5th = num => {
-  let roman = five.roman;
-  while (num > five.value) {
-    roman = roman.concat(one.roman);
-    num--;
-  }
-  return roman;
-};
-
-const roman10th = num => {
-  let roman = ten.roman;
-};
+const one = "I";
+const four = "IV";
+const five = "V";
+const nine = "IX";
+const ten = "X";
+const forty = "XL";
+const fifty = "L";
+const ninety = "XC";
+const hundred = "C";
+const fourHundred = "CD";
+const fiveHundred = "D";
+const nineHundred = "CM";
+const thousand = "M";
 
 const romanNum = num => {
-  for (let tk = one.value; tk <= thousand.value; tk++) {
-    if (num === one.value) {
-      return one.roman;
-    } else if (num > one.value && num < four.value) {
-      let count = one.roman;
-      while (num > 0) {
-        count = count.concat(one.roman);
-        num--;
-      }
-      return count;
-    } else if (num === four.value) {
-      return four.roman;
-    } else if (num === five.value) {
-      return five.roman;
-    } else if (num > five.value && num < nine.value) {
-      return roman5th(num);
-    } else if (num === nine.value) {
-      return nine.roman;
-    } else if (num === ten.value) {
-      return ten.roman;
-    } else if (num > ten.value && num < forty.value) {
-      let count = ten.roman;
-      while (forty.value / num > 0) {
-        count = count.concat(one.roman);
-        num--;
-      }
-      return count;
-    } else if (num === forty.value) {
-    } else if (num > forty.value && num < fifty.value) {
-    } else if (num === fifty.value) {
-    } else if (num > fifty.value && num < ninety.value) {
-    } else if (num === ninety.value) {
-    } else if (num > ninety.value && num < hundred.value) {
-    } else if (num === hundred.value) {
-    } else if (num > hundred.value && num < fourHundred.value) {
-    } else if (num === fourHundred.value) {
-    } else if (num > fourHundred.value && num < fiveHundred.value) {
-    } else if (num === fiveHundred.value) {
-    } else if (num > fiveHundred.value && num < nineHundred.value) {
-    } else if (num === nineHundred.value) {
-    } else if (num > nineHundred.value && num < thousand.value) {
-    } else if (num === thousand.value) {
+  let numeral = "";
+
+  if (num === 0) {
+    return "";
+  } else if (num < 4) {
+    while (num > 0) {
+      numeral += one;
+      num--;
     }
+    romanNum(num);
+  } else if (num === 4) {
+    numeral += four;
+  } else if (num === 5) {
+    numeral += five;
+  } else if (num < 9) {
+    numeral += five;
+    while (num > 5) {
+      numeral += one;
+      num--;
+    }
+    romanNum(num);
+  } else if (num === 9) {
+    numeral += nine;
+  } else if (num === 10) {
+    numeral += ten;
+  } else if (num < 40) {
+    let div = Math.floor(num / 10);
+    while (div > 0) {
+      numeral += ten;
+      div--;
+    }
+    numeral += romanNum(num % 10);
+  } else if (num === 40) {
+    numeral += forty;
+  } else if (num < 50) {
+    numeral += forty + romanNum(num % 40);
+  } else if (num === 50) {
+    numeral += fifty;
+  } else if (num < 90) {
+    numeral += fifty + romanNum(num % 50);
+  } else if (num === 90) {
+    numeral += ninety;
+  } else if (num < 100) {
+    numeral += ninety + romanNum(num % 90);
+  } else if (num === 100) {
+    numeral += hundred;
+  } else if (num < 400) {
+    let div = Math.floor(num / 100);
+    while (div > 0) {
+      numeral += hundred;
+      div--;
+    }
+    numeral += romanNum(num % 100);
+  } else if (num === 400) {
+    numeral += fourHundred;
+  } else if (num < 500) {
+    numeral += fourHundred + romanNum(num % 400);
+  } else if (num === 500) {
+    numeral += fiveHundred;
+  } else if (num < 900) {
+    let div = Math.floor(num / 500);
+    while (div > 0) {
+      numeral += fiveHundred;
+      div--;
+    }
+    numeral += romanNum(num % 500);
+  } else if (num === 900) {
+    numeral += nineHundred;
+  } else if (num < 1000) {
+    numeral += nineHundred + romanNum(num % 900);
+  } else if (num === 1000) {
+    numeral += thousand;
+  } else if (num < 4000) {
+    let div = Math.floor(num / 1000);
+    while (div > 0) {
+      numeral += thousand;
+      div--;
+    }
+    numeral += romanNum(num % 1000);
   }
+
+  return numeral;
 };
+
+window.addEventListener("load", () => {
+  const convertBtn = document.getElementById("convert-btn");
+  const number = document.getElementById("number");
+  let output = document.getElementById("output");
+
+  convertBtn.addEventListener("click", () => {
+    if (isNaN(parseInt(number.value))) {
+      output.innerText = "Please enter a valid number";
+      return;
+    }
+
+    if (parseInt(number.value) < 0) {
+      output.innerText = "Please enter a number greater than or equal to 1";
+      return;
+    }
+
+    if (parseInt(number.value) > 4000) {
+      output.innerText = "Please enter a number less than or equal to 3999";
+      return;
+    }
+
+    output.innerText = romanNum(parseInt(number.value));
+  });
+});
